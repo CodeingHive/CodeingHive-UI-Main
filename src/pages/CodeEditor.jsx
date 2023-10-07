6d0fe485aa33d61a4002913b02e35a07eab10fc2
Himasnhu-AT <aristech.official@gmail.com>
2023-10-07 17:11:42 +0530

    setup NAV

---
+++ src/pages/CodeEditor.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import DataProvider from './CodeEditorComponents/DataProvider';
+
+function CodeEditor() {
+    return (
+        <DataProvider />
+    );
+}
+
+export default CodeEditor;
