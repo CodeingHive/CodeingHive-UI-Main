0726e54c7fc316c7453c2c9d3087a39aa4943d32
Himanshu <117301124+Himasnhu-AT@users.noreply.github.com>
2023-10-08 17:56:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
         <hr />
 
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/CodeEditor" element={<CodeEditor />} />
+          <Route path="/" element={<CodeEditor />} />
+          <Route path="/CodeEditor" element={<Home />} />
         </Routes>
       </div>
     </Router>
